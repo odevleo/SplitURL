0422271ed82fd5655c83a6d9365ab6f4e8c1f775
Leonardo <leobragab@hotmail.com>
2021-10-11 15:46:31 -0300

    adcionando o main

---
+++ main.py
@@ -0,0 +1,16 @@
+def divide_url(split_url):
+    url_dividida = split_url.split('/')
+    return url_dividida
+
+
+url = 'https://github.com/pixies/calculadora-teste-py/releases/tag/v1.1'
+
+
+
+
+#lista = [url.split('/')]
+
+#print(lista)
+
+
+
