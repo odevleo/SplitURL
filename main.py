d3d7cd7b85740466f12370028132758b01dd677e
Leonardo <leobragab@hotmail.com>
2021-10-11 18:35:33 -0300

    update

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import requests
 from biblioteca import *
 
-repositorio = "https://github.com/pixies/calculadora-teste-py/releases/latest"
-arquivo_teste = "teste.txt"
 
-url = divide_url((pega_versao(repositorio)))
+usuario = input("Digite o usuario do repositório: ")
+repositorio = input("Digite o repositório: ")
 
-usuario = url[3]
-repositorio_ = url[4]
-ultima_versao = url[len(url)-1]
+print(pega_valor_da_versao_do_repo(usuario, repositorio))
 
-print("URL =", repositorio)
-print("Nome de Usuário =", usuario)
-print("Nome do Projeto =", repositorio_)
-print("Última Versão =", ultima_versao)
+dados = pega_valor_da_versao_do_repo(usuario, repositorio)
 
-salvar = ["REPO:", usuario + '/' + repositorio_, "VERSION:", ultima_versao]
-cria_arquivo(arquivo_teste)
-add_arquivo(arquivo_teste, str(salvar))
-print(read_arquivo(arquivo_teste))
+banco_existe_senao_crie('dados_repo')
+
+guardar_dados('dados repo', dados)
+
+
+
 
 
 
