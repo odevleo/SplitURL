dd419ed995e57266a1dc5e89d89eea8cd308926b
Leonardo <leobragab@hotmail.com>
2021-10-11 16:17:56 -0300

    update

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-def divide_url(split_url):
-    url_dividida = split_url.split('/')
-    return url_dividida
+import requests
+from biblioteca import *
 
+repositorio = "https://github.com/pixies/calculadora-teste-py/releases/latest"
+arquivo_teste = "teste.txt"
 
-url = 'https://github.com/pixies/calculadora-teste-py/releases/tag/v1.1'
+url = divide_url((pega_versao(repositorio)))
 
+usuario = url[3]
+repositorio_ = url[4]
+ultima_versao = url[len(url)-1]
 
+print("URL =", repositorio)
+print("Nome de Usuário =", usuario)
+print("Nome do Projeto =", repositorio_)
+print("Última Versão =", ultima_versao)
 
+salvar = ["REPO:", usuario + '/' + repositorio_, "VERSION:", ultima_versao]
+cria_arquivo(arquivo_teste)
+add_arquivo(arquivo_teste, str(salvar))
+print(read_arquivo(arquivo_teste))
 
-#lista = [url.split('/')]
-
-#print(lista)
 
 
 
